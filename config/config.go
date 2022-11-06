e02b7ffd3bab444e359434b93e4fa6e447457bff
shahar <shahar.cohen@appsflyer.com>
2022-11-06 13:18:16 +0200

    commented err handling .env file

--- config/config.go
+++ config/config.go
@@ -2,15 +2,14 @@ package config
 
 import (
 	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
 func EnvMongoURI() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
+	//if err != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
 
 	return os.Getenv("MONGOURI")
 }
